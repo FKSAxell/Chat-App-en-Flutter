abd61f774934522213fc391bb93a9bbea1100992
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-04-24 00:48:11 -0500

    Base fin

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,10 +2,18 @@ import 'dart:io';
 
 class Enviroment {
   static String apiUrl = Platform.isAndroid
-      ? 'https://piogram.azurewebsites.net/api'
-      : 'https://piogram.azurewebsites.net/api';
+      ? 'http://192.168.1.15:3000/api'
+      : 'http://192.168.1.15:3000/api';
 
   static String socketUrl = Platform.isAndroid
-      ? 'https://piogram.azurewebsites.net/'
-      : 'https://piogram.azurewebsites.net/';
+      ? 'http://192.168.1.15:3000/'
+      : 'http://192.168.1.15:3000/';
+
+  // static String apiUrl = Platform.isAndroid
+  //     ? 'https://piogram.azurewebsites.net/api'
+  //     : 'https://piogram.azurewebsites.net/api';
+
+  // static String socketUrl = Platform.isAndroid
+  //     ? 'https://piogram.azurewebsites.net/'
+  //     : 'https://piogram.azurewebsites.net/';
 }
